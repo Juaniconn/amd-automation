7657bacfb588cbf60c8a0017648c2d38e46444c8
Juan Ponce <me.juanponce@gmail.com>
2020-05-09 13:28:54 -0600

    Updates

---
+++ precache-manifest.ef8ff9cc9642d684febcac35ffa1949d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08244651125fe9ec2f3db994c82195f4",
+    "url": "/amd-automation/index.html"
+  },
+  {
+    "revision": "af94010164a8b26b3304",
+    "url": "/amd-automation/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "81bd683f9e7378095916",
+    "url": "/amd-automation/static/js/2.6941dce8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/amd-automation/static/js/2.6941dce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af94010164a8b26b3304",
+    "url": "/amd-automation/static/js/main.610ada62.chunk.js"
+  },
+  {
+    "revision": "7d624e2399c5dc602c95",
+    "url": "/amd-automation/static/js/runtime-main.d2e81722.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/amd-automation/static/media/logo.5d5d9eef.svg"
+  }
+]);
