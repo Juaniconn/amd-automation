eaf5f231c15e946ee49e8a63a14026a5324a8524
Juan Ponce <me.juanponce@gmail.com>
2020-05-11 21:05:04 -0600

    Updates

---
+++ precache-manifest.3a171a9c666372063e181f2f1f8c6c15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dbd254fd3befe3b1b645d6ac561d5e8",
+    "url": "/amd-automation/index.html"
+  },
+  {
+    "revision": "745fe4e5a3950460cb6e",
+    "url": "/amd-automation/static/js/2.463961cb.chunk.js"
+  },
+  {
+    "revision": "943aec86c35bb3f39d18124af76a452f",
+    "url": "/amd-automation/static/js/2.463961cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "317e6617988906a6c561",
+    "url": "/amd-automation/static/js/main.99f68145.chunk.js"
+  },
+  {
+    "revision": "7d624e2399c5dc602c95",
+    "url": "/amd-automation/static/js/runtime-main.d2e81722.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/amd-automation/precache-manifest.400074b16f00c66ce6cd89ef79d0c339.js"
+  "/amd-automation/precache-manifest.3a171a9c666372063e181f2f1f8c6c15.js"
 );
 
 self.addEventListener('message', (event) => {
