4389382eb710a2addb96229e7cf2cb508326bdfc
Juan Ponce <me.juanponce@gmail.com>
2020-07-03 16:54:20 -0600

    Updates

---
+++ precache-manifest.21e69fbe5952f8abde6992925ee38598.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504885cd970b3b3c374cda6dad49aecb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9f0ce390df37e18a83b",
+    "url": "/static/js/2.4de20642.chunk.js"
+  },
+  {
+    "revision": "c227e2fa14826bea2472fceff57bc67a",
+    "url": "/static/js/2.4de20642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dec8eb14999df00bf275",
+    "url": "/static/js/main.35a7db44.chunk.js"
+  },
+  {
+    "revision": "ef1ab57689284fbdb52f",
+    "url": "/static/js/runtime-main.e5be43dd.js"
+  }
+]);
